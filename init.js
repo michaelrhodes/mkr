234abad689cd62dad700eada49443f1522af2ff4
michaelrhodes <spam@michaelrhod.es>
2018-02-03 13:42:00 +1100

    1.0.2

--- init.js
+++ init.js
@@ -1,10 +1,10 @@
 var fs = require('fs')
 var path = require('path')
-var root = require('module-root')(process.cwd())
 var stringify = require('./util/stringify')
 var filepath = require('./util/filepath')
 var write = require('./util/write')
 var repo = require('./util/repo')
+var cwd = process.cwd()
 
 module.exports = init
 
@@ -12,11 +12,11 @@ function init () {
   if (fs.existsSync(filepath)) return
 
   var pkg = {
-    name: path.basename(root),
+    name: path.basename(cwd),
     author: 'Michael Rhodes',
     version: '0.0.0',
     main: 'index.js',
-    repository: repo(root),
+    repository: repo(cwd),
     license: 'MIT'
   }
 
